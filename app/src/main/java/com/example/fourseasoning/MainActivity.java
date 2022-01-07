cd9112b207dc0524264f6229fad39c261e9c96c8
Wu Lezheng <wulezheng00@gmail.com>
2022-01-07 12:40:51 +0800

    Added fragments, logos. Modified Main Activity

--- app/src/main/java/com/example/fourseasoning/MainActivity.java
+++ app/src/main/java/com/example/fourseasoning/MainActivity.java
@@ -1,16 +1,47 @@
 package com.example.fourseasoning;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setBackground(null);
+        getSupportFragmentManager().beginTransaction().replace(R.id.bottom_container, new HomeFragment()).commit();
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        fragment = new HomeFragment();
+                        break;
+                    case  R.id.bluetooth:
+                        fragment = new BluetoothFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.bottom_container, fragment).commit();
+                return true;
+            }
+        });
+
     }
 
-    // write sth
 }
