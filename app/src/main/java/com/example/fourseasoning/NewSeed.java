8625cee4178809089aa847d6a09b3be9e02ec56e
CShuwen <96762739+CShuwen@users.noreply.github.com>
2022-01-07 16:32:40 +0800

    add recycler view

---
+++ app/src/main/java/com/example/fourseasoning/NewSeed.java
@@ -0,0 +1,57 @@
+package com.example.fourseasoning;
+
+public class NewSeed {
+    private String SeedName;
+    private String SeedCondition;
+    private String LifeCycleTitle;
+    private String SeedLifeCycle;
+    private int Thumbnail;
+
+    public NewSeed(String name, String seedCondition, String lifeCycleTitle, String seedLifeCycle, int thumbnail){
+        SeedName = name;
+        SeedCondition = seedCondition;
+        LifeCycleTitle = lifeCycleTitle;
+        SeedLifeCycle = seedLifeCycle;
+        Thumbnail = thumbnail;
+    }
+
+    public String getSeedName() {
+        return SeedName;
+    }
+
+    public void setSeedName(String seedName) {
+        SeedName = seedName;
+    }
+
+    public String getSeedCondition() {
+        return SeedCondition;
+    }
+
+    public void setSeedCondition(String seedCondition) {
+        SeedCondition = seedCondition;
+    }
+
+    public String getSeedLifeCycle() {
+        return SeedLifeCycle;
+    }
+
+    public void setSeedLifeCycle(String seedLifeCycle) {
+        SeedLifeCycle = seedLifeCycle;
+    }
+
+    public String getLifeCycleTitle() {
+        return LifeCycleTitle;
+    }
+
+    public void setLifeCycleTitle(String lifeCycleTitle) {
+        LifeCycleTitle = lifeCycleTitle;
+    }
+
+    public int getThumbnail() {
+        return Thumbnail;
+    }
+
+    public void setThumbnail(int thumbnail) {
+        Thumbnail = thumbnail;
+    }
+}
