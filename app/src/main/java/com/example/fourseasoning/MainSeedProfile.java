3208ba466b33400aac75f8220b8ad677adb86839
CShuwen <96762739+CShuwen@users.noreply.github.com>
2022-01-07 11:17:50 +0800

    cshuwen

---
+++ app/src/main/java/com/example/fourseasoning/MainSeedProfile.java
@@ -0,0 +1,10 @@
+package com.example.fourseasoning;
+
+import android.os.Bundle;
+
+public class MainSeedProfile extends MainActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
