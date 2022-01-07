f55a89448c23ce8bb04f31e43c11b57f75973201
CShuwen <96762739+CShuwen@users.noreply.github.com>
2022-01-07 19:14:22 +0800

    add function

--- app/src/main/java/com/example/fourseasoning/MainActivity.java
+++ app/src/main/java/com/example/fourseasoning/MainActivity.java
@@ -3,20 +3,26 @@ package com.example.fourseasoning;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.fourseasoning.add.MainNewSeedList;
 import com.example.fourseasoning.bluetooth.BluetoothFragment;
 import com.example.fourseasoning.home.HomeFragment;
 import com.example.fourseasoning.home.MainSeedProfile;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+    FragmentContainerView fragmentContainerView;
+    FloatingActionButton addPlantButton;
 
 
     @Override
@@ -26,8 +32,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setBackground(null);
-        getSupportFragmentManager().beginTransaction().replace(R.id.bottom_container, new HomeFragment()).commit();
+        addPlantButton = findViewById(R.id.add_plant);
+        fragmentContainerView = findViewById(R.id.fragment_container);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MainSeedProfile()).commit();
         bottomNavigationView.setSelectedItemId(R.id.home);
+
+        addPlantButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MainNewSeedList()).commit();
+            }
+        });
 
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
@@ -38,13 +53,18 @@ public class MainActivity extends AppCompatActivity {
                         fragment = new MainSeedProfile();
                         break;
                     case  R.id.bluetooth:
+
                         fragment = new BluetoothFragment();
                         break;
+                        /*
                     case R.id.add:
+                        Toast.makeText(getApplicationContext(), "add clicked", Toast.LENGTH_SHORT).show();
                         fragment = new MainNewSeedList();
                         break;
+                        */
+
                 }
-                getSupportFragmentManager().beginTransaction().replace(R.id.bottom_container, fragment).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
                 return true;
             }
         });
--- app/src/main/java/com/example/fourseasoning/add/MainNewSeedList.java
+++ app/src/main/java/com/example/fourseasoning/add/MainNewSeedList.java
@@ -21,12 +21,12 @@ public class MainNewSeedList extends Fragment {
     RecyclerView myrecyclerView;
     RecyclerViewAdaptor_newseedlist myAdapter;
     List<NewSeed> seed1;
-    Context thiscontext;
+    Context thisContext;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add, container, false);
-        thiscontext = container.getContext();
+        thisContext = container.getContext();
         seed1 = new ArrayList<>();
         seed1.add(new NewSeed("Peppermint", "Soil Condition : Alkaline\n"
                 + "water frequency : daily / Once per 24 Hours\n"
@@ -46,12 +46,62 @@ public class MainNewSeedList extends Fragment {
                 + "preferred location : Low Light",
                 "Life Cycle",
                 "A Week to Full Maturity", R.drawable.bean_sprout));
+        seed1.add(new NewSeed("Mimosa", "Soil Condition : acidic to neutral (5.0-7.5)\n"
+                + "water frequency : Daily\n"
+                + "water method : Mist\n"
+                + "preferred location : Bright light, some direct sunlight",
+                "Life Cycle",
+                "Three months to Full Maturity", R.drawable.bean_sprout));
+        seed1.add(new NewSeed("catgrass", "Soil Condition : neutral \n"
+                + "water frequency : Daily\n"
+                + "water method : Mist\n"
+                + "preferred location : Leave it out of direct sunlight",
+                "Life Cycle",
+                "Half a Month to Full Maturity", R.drawable.bean_sprout));
+        seed1.add(new NewSeed("basil", "Soil Condition : slightly acidic to slightly alkaline\n"
+                + "water frequency : Daily\n"
+                + "water method : pour 1 inch\n"
+                + "preferred location : Full Sun\n",
+                "Life Cycle",
+                "10 days to Full Maturity", R.drawable.bean_sprout));
+        seed1.add(new NewSeed("ginger", "Soil Condition : Acidic\n"
+                + "water frequency : Daily\n"
+                + "water method : pour 1 inch\n"
+                + "preferred location : Full Sun\n",
+                "Life Cycle",
+                "4 - 7 Months to Full Maturity", R.drawable.bean_sprout));
+        seed1.add(new NewSeed("oregano", "Soil Condition : Slightly acidic to neutral (6.5 to 7.0)\n"
+                + "water frequency : Daily\n"
+                + "water method : pour 1 inch\n"
+                + "preferred location : Full Sun\n",
+                "Life Cycle",
+                "1.5 Months to Full Maturity", R.drawable.bean_sprout));
+        seed1.add(new NewSeed("rosemary", "Soil Condition : Acidic, neutral (6.0 to 7.0)\n"
+                + "water frequency : Daily\n"
+                + "water method : pour 1 inch\n"
+                + "preferred location : Full Sun\n",
+                "Life Cycle",
+                "6 - 12 Months to Full Maturity", R.drawable.bean_sprout));
+        seed1.add(new NewSeed("spadeleaf", "Soil Condition : Neutral to slightly alkaline (6-7.8)\n"
+                + "water frequency : Daily\n"
+                + "water method : pour 1 inch\n"
+                + "preferred location : Full Sun\n",
+                "Life Cycle",
+                "3 Months to Full Maturity", R.drawable.bean_sprout));
+        seed1.add(new NewSeed("zebra plant", "Soil Condition : Neutral\n"
+                + "water frequency : Daily\n"
+                + "water method : Water when the top nch of soil has dried out\n"
+                + "preferred location : 4 - 6 hours of non-direct Sunlight\n",
+                "Life Cycle",
+                "6 Months to Full Maturity", R.drawable.bean_sprout));
+
+
 
         myrecyclerView = view.findViewById(R.id.recyclerView_add_id);
 
-        myAdapter = new RecyclerViewAdaptor_newseedlist(thiscontext, seed1);
+        myAdapter = new RecyclerViewAdaptor_newseedlist(thisContext, seed1);
 
-        myrecyclerView.setLayoutManager(new GridLayoutManager(thiscontext, 2));
+        myrecyclerView.setLayoutManager(new GridLayoutManager(thisContext, 2));
 
         myrecyclerView.setAdapter(myAdapter);
 
